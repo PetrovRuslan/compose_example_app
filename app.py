e7d08c9412c3328d9fdacef92ceb6414e8042a43
Ruslan Petrov <rpetrov@Ruslans-MacBook-Air.local>
2023-12-06 15:27:10 +0200

    первый слайд

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import redis
+import time
 
 app = Flask(__name__)
 
@@ -23,24 +24,5 @@ def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
-@app.route('/toggle')
-def toggle():
-    global health_status
-    health_status = not health_status
-    return jsonify(health_value=health_status)
-
-
-@app.route('/health')
-def health():
-    if health_status:
-        resp = jsonify(health="healthy")
-        resp.status_code = 200
-    else:
-        resp = jsonify(health="unhealthy")
-        resp.status_code = 500
-
-    return resp
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
